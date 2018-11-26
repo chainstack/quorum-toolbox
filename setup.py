b68a39ade9545f70cb2b73da134ce7e16249a35e
Alex Khaerov <hayorov@users.noreply.github.com>
2018-11-26 18:12:59 +0800

    Copy non-py files during installation (#6)
    * Find and define any packages automatically
    
    * Version bump
    
    * Correct Manifest
    
    * Copy non-python files during installation

--- setup.py
+++ setup.py
@@ -10,10 +10,11 @@ def read(fname):
 
 
 setup(name='quorumtoolbox',
-      version='1.0.2',
+      version='1.0.6',
       description='Quorum-toolbox',
       long_description=read('README.md'),
       packages=find_packages(),
+      include_package_data=True,
       install_requires=[
           'requests>=2.20.0',
           'sh>=1.12.14',
